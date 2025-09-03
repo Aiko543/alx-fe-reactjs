67a8eebf186bcd2fae41ac6860e7b66f6a3904ea
Aiko543 <njokistephanie01@gmail.com>
2025-09-03 20:40:18 +0300

    1. Create a Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +25,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Counter />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
