e13668fed35422a580b631537b76741a9819b8f3
Aiko543 <njokistephanie01@gmail.com>
2025-09-03 20:35:04 +0300

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: 'lightgray' }}>
+      <p style={{ fontStyle: 'italic' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
